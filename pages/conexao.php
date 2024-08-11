14a61d8ca895e94205061e2436e3405b0c69a63d
Daniel Calebe <160525339+danielcalebe@users.noreply.github.com>
2024-08-11 12:20:38 -0300

    Conexao

--- pages/conexao.php
+++ pages/conexao.php
@@ -1,14 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "senaiflix_dCalebe";
+$servername = "mysql-2221c92b-danielcalebe719-2b82.l.aivencloud.com"; 
+$username = "avnadmin"; 
+$password = "AVNS_MRgcH_4ZC0MTrUnxZpv";
+$dbname = "flix"; 
 
-// Create connection
+// Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+// Verifica a conexão
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
+    echo "Conexão bem-sucedida!";
 }
+
+// Fechar a conexão quando terminar
+$conn->close();
+?>
